a759af5cd876ecf725ffc54ed8e176ff0de6ed18
yonaMac <yonaimyo@gmail.com>
2025-03-23 14:58:23 +0900

    Feat: 상수 전처리용 헤더 constant.hpp 추가

---
+++ include/constant.hpp
@@ -0,0 +1,10 @@
+#ifndef CONSTANT_HPP
+#define CONSTANT_HPP
+
+// allowed error range
+#define EPSILON 1e-6f
+
+// infiniy value
+#define FLT_INF std::numeric_limits<float>::infinity()
+
+#endif
--- include/settings.hpp
+++ include/settings.hpp
@@ -5,7 +5,4 @@
 #define SCR_WIDTH	512
 #define SCR_HEIGHT	512
 
-// allowed error range
-#define EPSILON		1e-6f
-
 #endif
