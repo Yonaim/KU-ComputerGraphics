9f6b8e7a30863aae9ce47fd611c51192d0782592
yonaMac <yonaimyo@gmail.com>
2025-03-23 19:23:26 +0900

    Feat: 렌더 로직 스켈레톤 코드 추가

--- src/render.cpp
+++ src/render.cpp
@@ -9,14 +9,29 @@ the pixel’s color to white; otherwise, set the pixel’s color to
 black.
 – See the Figure for a reference image.
 */
+
 #include "Scene.hpp"
+#include "constant.hpp"
 #include "settings.hpp"
 
-void render()
+/*
+	1. 레이 생성 (카메라 위치 - 현 픽셀 중심 좌표)
+	2. intersection 검사, 가장 가까운 t 찾기
+	2. 검사 결과에 따라 흰색 혹은 검은색으로
+*/
+void render(Scene &scene, Camera &camera)
 {
+	glm::vec3 color;
+	Ray       ray;
 
-	// 픽셀의 수만큼 반복
-	for (int i = 0; i < SCR_WIDTH * SCR_HEIGHT; i++)
+	for (int y = 0; y < SCR_HEIGHT; y++)
 	{
+		for (int x = 0; x < SCR_WIDTH; x++)
+		{
+			ray   = camera.getRay(x, y);
+			color = scene.trace(ray, 0, FLT_INF);
+			// TODO: 텍스처에 픽셀 컬러 담기
+		}
 	}
+	// TODO: 스크린 프레임버퍼에 텍스처를 렌더링
 }
