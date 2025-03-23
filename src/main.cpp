6734e9104cd1c2efe71c381dc67127a7acd4e262
yonaMac <yonaimyo@gmail.com>
2025-03-24 04:49:17 +0900

    Refactor: main.cpp 코드 정리

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,138 @@
-#include "graphics.h"
+#include "Plane.hpp"
+#include "Renderer.hpp"
+#include "Scene.hpp"
+#include "Sphere.hpp"
+#include "settings.h"
 
-void render_loop(GLFWwindow *window)
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#else
+#include <GL/glu.h>
+#endif
+
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+#define GLFW_INCLUDE_GLU
+#define GLFW_DLL
+#include <GLFW/glfw3.h>
+#include <vector>
+
+#define GLM_SWIZZLE
+#include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+void initScene(Scene &scene)
 {
-	// render loop
+	/*
+	Camera
+	– Assume a perspective camera
+	– eye point at e = (0, 0, 0)
+	– orientation given by u = (1, 0, 0), v = (0, 1, 0) and w = (0, 0, 1).
+	– (Note that the camera is looking along the direction −w.)
+	– Assume that the viewing region on the image plane is defined by
+		l = −0.1, r = 0.1, b = −0.1, t = 0.1, and d = 0.1.
+	*/
+	scene.setCameraImagePlane(-0.1, 0.1, -0.1, 0.1);
+	scene.setCameraEye(glm::vec3(0, 0, 0));
+	scene.setCameraDistance(0.1);
+	scene.setCameraDirection(glm::vec3(1, 0, 0), glm::vec3(0, 1, 0),
+							 glm::vec3(0, 0, 1));
+
+	/*
+	 The scene consists of the following four objects:
+	 – Plane P , with equation y = −2.
+	 – Sphere S1, with center at (−4, 0, −7) and radius 1.
+	 – Sphere S2, with center at (0, 0, −7) and radius 2.
+	 – Sphere S3, with center at (4, 0, −7) and radius 1.
+	*/
+	scene.addSurface(Plane(glm::vec3(0, -2, 0), glm::vec3(0, 1, 0)));
+	scene.addSurface(Sphere(glm::vec3(-4, 0, -7), 1));
+	scene.addSurface(Sphere(glm::vec3(0, 0, -7), 2));
+	scene.addSurface(Sphere(glm::vec3(4, 0, -7), 1));
+}
+
+static void keyCallback(GLFWwindow *window, int key, int scancode, int action,
+						int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+void initGLFW(GLFWwindow **window)
+{
+	if (!glfwInit())
+		return;
+
+	// use openGL 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	// create window and use
+	*window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT,
+							   "Ray-Tracer (Assignment 1)", NULL, NULL);
+	if (!*window)
+	{
+		glfwTerminate();
+		return;
+	}
+	glfwMakeContextCurrent(*window);
+	glfwSetKeyCallback(*window, keyCallback);
+}
+
+void renderFrameLoop(Renderer &renderer, GLFWwindow *window)
+{
+	bool needRecompute = true;
+
 	while (!glfwWindowShouldClose(window))
 	{
-		processInput(window);
-
-		// render
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// glfw: swap buffers and poll IO events (keys pressed/released, mouse
-		// moved etc.)
+		if (needRecompute)
+		{
+			renderer.rayTrace();
+			needRecompute = false;
+		}
+
+		glEnable(GL_TEXTURE_2D);
+		glBindTexture(GL_TEXTURE_2D, renderer.textureID);
+
+		glBegin(GL_QUADS);
+		glTexCoord2f(0.0f, 0.0f);
+		glVertex2f(-1.0f, -1.0f);
+		glTexCoord2f(1.0f, 0.0f);
+		glVertex2f(1.0f, -1.0f);
+		glTexCoord2f(1.0f, 1.0f);
+		glVertex2f(1.0f, 1.0f);
+		glTexCoord2f(0.0f, 1.0f);
+		glVertex2f(-1.0f, 1.0f);
+		glEnd();
+
+		glDisable(GL_TEXTURE_2D);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 }
-
 int main()
 {
-	Scene scene;
-	init_scene(scene);
+	Renderer    renderer;
+	GLFWwindow *window;
 
-	GLFWwindow *window = init_glfw();
+	initScene(renderer.scene);
+	initGLFW(&window);
 	if (window == NULL)
 		return (1);
 
-	if (!init_glad())
-		return (1);
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
-	render_loop(window);
+	// loop
+	renderFrameLoop(renderer, window);
 
-	// glfw: terminate, clearing all previously allocated GLFWresources.
+	// terminate
+	glfwDestroyWindow(window);
 	glfwTerminate();
 	return (0);
 }
