e6f935d32dd5ffed768073bdb9896d5916b5ef15
yonaMac <yonaimyo@gmail.com>
2025-03-23 22:01:26 +0900

    Feat: hitResult 구조체 멤버 추가

--- include/hitResult.hpp
+++ include/hitResult.hpp
@@ -1,12 +1,15 @@
 #ifndef HITRESULT_HPP
 #define HITRESULT_HPP
 
+#include <glm/glm.hpp>
 class Surface;
 
 typedef struct
 {
-	Surface *surface;
-	float    t;
+	Surface  *surface;
+	float     t;
+	glm::vec3 normal;
+	glm::vec3 point;
 } hitResult;
 
 #endif
