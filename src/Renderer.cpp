9b2741fe4f46255a99c9a4521ebc11ecf5f84982
yonaMac <yonaimyo@gmail.com>
2025-03-25 00:29:40 +0900

    Refactor: Renderer 클래스 멤버 구성 변경
    - setupTexture() 함수 삭제
    - output unsigned char 벡터로 변경

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -7,35 +7,11 @@
 
 Renderer::Renderer()
 {
-	memset(output, 0, SCR_WIDTH * SCR_HEIGHT * 3);
-	this->textureID = 0;
+	this->output.resize(SCR_WIDTH * SCR_HEIGHT * 3);
 }
 
 Renderer::~Renderer()
 {
-	glDeleteTextures(1, &textureID);
-}
-
-void Renderer::setupTexture()
-{
-	std::cout << "Setting up texture..." << std::endl;
-
-	memset(output, 0, sizeof(output));
-
-	glGenTextures(1, &textureID);
-	std::cout << "Generated texture ID: " << textureID << std::endl;
-
-	glBindTexture(GL_TEXTURE_2D, textureID);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB,
-				 GL_UNSIGNED_BYTE, output);
-
-	std::cout << "Texture setup successful" << std::endl;
 }
 
 /*
