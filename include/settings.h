884b5edcc16a05676ea6c3c6db5c1fd20d3de494
yonaMac <yonaimyo@gmail.com>
2025-03-25 00:30:03 +0900

    Feat: resize 콜백 함수 추가

--- include/settings.h
+++ include/settings.h
@@ -4,11 +4,12 @@
 #include "gl_core.h"
 
 // the image resolution is 512 × 512
-#define SCR_WIDTH 512
+#define SCR_WIDTH  512
 #define SCR_HEIGHT 512
 
 void errorCallback(int error, const char *description);
 void keyCallback(GLFWwindow *window, int key, int scancode, int action,
 				 int mods);
+void resizeCallback(GLFWwindow *window, int width, int height);
 
 #endif
