1e6182cd4aeecdf1d7dbb420fce08913fe09a530
yonaMac <yonaimyo@gmail.com>
2025-03-27 17:25:05 +0900

    Feat: Material 클래스 추가

--- include/Material.hpp
+++ include/Material.hpp
@@ -1,12 +1,20 @@
 #ifndef MATERIAL_HPP
 #define MATERIAL_HPP
 
+#include <glm/glm.hpp>
+
+// phong reflection model
 class Material
 {
-private:
-	/* data */
-public:
+  private:
+	glm::vec3 ka;        // ambient
+	glm::vec3 kd;        // diffuse
+	glm::vec3 ks;        // specular
+	float     shininess; // specular power
+
+  public:
 	Material(/* args */);
+	Material(glm::vec3 ka, glm::vec3 kd, glm::vec3 ks, float shininess);
 	~Material();
 };
 
