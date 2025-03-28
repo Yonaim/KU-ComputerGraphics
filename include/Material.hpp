38d611fd4c50c10fd5af69f3d294ea75d1a2582e
yonaMac <yonaimyo@gmail.com>
2025-03-29 00:11:49 +0900

    Feat: Material 클래스 phong 모델 계산 함수 구현

--- include/Material.hpp
+++ include/Material.hpp
@@ -12,10 +12,19 @@ class Material
 	glm::vec3 kd;        // diffuse
 	glm::vec3 ks;        // specular
 	float     shininess; // specular power
-	
+
 	Material(/* args */);
 	Material(glm::vec3 ka, glm::vec3 kd, glm::vec3 ks, float shininess);
 	~Material();
+
+	glm::vec3 calculateAmbient(const glm::vec3 &lightColor) const;
+	glm::vec3 calculateDiffuse(const glm::vec3 &lightColor,
+							   const glm::vec3 &normal,
+							   const glm::vec3 &lightDirection) const;
+	glm::vec3 calculateSpecular(const glm::vec3 &lightColor,
+								const glm::vec3 &normal,
+								const glm::vec3 &lightDirection,
+								const glm::vec3 &viewDirection) const;
 };
 
 #endif
--- src/Material.cpp
+++ src/Material.cpp
@@ -12,3 +12,28 @@ Material::Material(glm::vec3 ka, glm::vec3 kd, glm::vec3 ks, float shininess)
 	: ka(ka), kd(kd), ks(ks), shininess(shininess)
 {
 }
+
+// Ambient lighting calculation
+glm::vec3 Material::calculateAmbient(const glm::vec3 &lightColor) const
+{
+	return (ka * lightColor);
+}
+
+// Diffuse lighting calculation
+glm::vec3 Material::calculateDiffuse(const glm::vec3 &lightColor,
+									 const glm::vec3 &normal,
+									 const glm::vec3 &lightDirection) const
+{
+	return (kd * lightColor * glm::max(glm::dot(normal, lightDirection), 0.0f));
+}
+
+// Specular lighting calculation
+glm::vec3 Material::calculateSpecular(const glm::vec3 &lightColor,
+									  const glm::vec3 &normal,
+									  const glm::vec3 &lightDirection,
+									  const glm::vec3 &viewDirection) const
+{
+	glm::vec3 h = glm::normalize(lightDirection + viewDirection);
+	return (ks * lightColor
+			* glm::pow(glm::max(glm::dot(normal, h), 0.0f), shininess));
+}
